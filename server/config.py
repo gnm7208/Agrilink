361d55b93620bc6c6954a8301d807ab540380b77
George <george@example.com>
2026-01-27 14:14:14 +0300

    `Added initial configuration file for server`

---
+++ server/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://localhost/agrilink"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
